9a608c8bb7b5dd8229a38547411b2e382a670921
Randall J. Russell Hernadez <rjrussellh03@gmail.com>
2022-05-14 00:39:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Gamer", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Gamer", "Future Vet", "Future Game Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Gamer", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Gamer", "Developer", "Future Vet", "Future Game Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
