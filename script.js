a0bce3495bd3cd0825913c542401f1028ea201e8
Randall J. Russell Hernadez <rjrussellh03@gmail.com>
2022-05-14 00:51:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Gamer", "Developer", "Future Vet", "Future Game Developer"],
+        strings: ["Gamer", "Future Vet", "Future Game Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
